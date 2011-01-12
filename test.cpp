b0ed85699b48637542090f859b5f6ea6e9c9b31e
Vadim Madgazin <vrm@vmgames.com>
2011-01-12 03:06:24 -0800

    edited onlyne!

--- test.cpp
+++ test.cpp
@@ -4,11 +4,11 @@ const wchar_t* ProgramTitle = L"Dichotic Harmony Files Converter";
 const wchar_t* ProgramCopyright = L"Copyright (c) 2010 Vadim Madgazin";
 const wchar_t* VER_NUM = L"1.00";
 
-// ������ �� 2010 �.
+// русский - UTF-8
 
 int midi_tracks_number = 4;
 
-int clks_per_beat = 1000; // ������ 100...1000
+int clks_per_beat = 1000; // 100...1000
 
 
 void main(int argc, char **argv)
